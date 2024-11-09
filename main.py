5768e71a97ec266ffd1082f73f2510e4ab8afaf0
OfficialDex <140303092+OfficialDex@users.noreply.github.com>
2024-11-09 06:09:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -287,6 +287,7 @@ def main_page():
         </body>
     </html>
     """
-    if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    
+if __name__ == '__main__':
+app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
     
